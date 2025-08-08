bf989c077fb95eb6b99e790ef09e10e8d812f078
Entropy1110 <entropy1110@gmail.com>
2025-08-08 05:18:36 +0000

    ignore

--- host/target/aarch64-unknown-linux-gnu/release/deps/proto-94357514f8a7447e.d
+++
@@ -1,9 +0,0 @@
-/root/incubator-teaclave-trustzone-sdk/examples/enc_mnist-rs/host/target/aarch64-unknown-linux-gnu/release/deps/proto-94357514f8a7447e.d: /root/incubator-teaclave-trustzone-sdk/examples/enc_mnist-rs/proto/src/lib.rs /root/incubator-teaclave-trustzone-sdk/examples/enc_mnist-rs/proto/src/inference.rs /root/incubator-teaclave-trustzone-sdk/examples/enc_mnist-rs/proto/src/../../ta/inference/uuid.txt
-
-/root/incubator-teaclave-trustzone-sdk/examples/enc_mnist-rs/host/target/aarch64-unknown-linux-gnu/release/deps/libproto-94357514f8a7447e.rlib: /root/incubator-teaclave-trustzone-sdk/examples/enc_mnist-rs/proto/src/lib.rs /root/incubator-teaclave-trustzone-sdk/examples/enc_mnist-rs/proto/src/inference.rs /root/incubator-teaclave-trustzone-sdk/examples/enc_mnist-rs/proto/src/../../ta/inference/uuid.txt
-
-/root/incubator-teaclave-trustzone-sdk/examples/enc_mnist-rs/host/target/aarch64-unknown-linux-gnu/release/deps/libproto-94357514f8a7447e.rmeta: /root/incubator-teaclave-trustzone-sdk/examples/enc_mnist-rs/proto/src/lib.rs /root/incubator-teaclave-trustzone-sdk/examples/enc_mnist-rs/proto/src/inference.rs /root/incubator-teaclave-trustzone-sdk/examples/enc_mnist-rs/proto/src/../../ta/inference/uuid.txt
-
-/root/incubator-teaclave-trustzone-sdk/examples/enc_mnist-rs/proto/src/lib.rs:
-/root/incubator-teaclave-trustzone-sdk/examples/enc_mnist-rs/proto/src/inference.rs:
-/root/incubator-teaclave-trustzone-sdk/examples/enc_mnist-rs/proto/src/../../ta/inference/uuid.txt:
--- host/target/release/build/serde-89850a824e513000/build_script_build-89850a824e513000.d
+++
@@ -1,5 +0,0 @@
-/root/teaclave_sdk_src/examples/enc_mnist-rs/host/target/release/build/serde-89850a824e513000/build_script_build-89850a824e513000.d: /root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/serde-1.0.219/build.rs
-
-/root/teaclave_sdk_src/examples/enc_mnist-rs/host/target/release/build/serde-89850a824e513000/build_script_build-89850a824e513000: /root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/serde-1.0.219/build.rs
-
-/root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/serde-1.0.219/build.rs:
